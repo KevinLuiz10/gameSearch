3f2220174758dd9feb31b685bc949e7022d6e47f
Kevin Luiz <kevin37614250@hotmail.com>
2025-11-29 03:18:26 -0300

    Creation of the file start.sh, to run the docker initialization script in linux

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Derrubando containers antigos..."
+docker-compose down
+
+echo "Construindo e subindo o ambiente de desenvolvimento..."
+docker-compose up --build
+
+# O comando 'read' abaixo simula o 'pause' do Windows
+read -p "Pressione [Enter] para sair..."
