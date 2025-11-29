77b9c95e656273a13bf22d27a07ef95442edcc42
Kevin Luiz <kevin37614250@hotmail.com>
2025-11-29 03:05:42 -0300

    vite-plugin-compression configuration, for compression of static achives in frontend.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,47 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       '/api': {
-//         target: 'https://www.freetogame.com',
-//         changeOrigin: true,
-//         secure: false,
-//         rewrite: (path) => path.replace(/^\/api/, '/api')
-//       },
-//     },
-//   },
-// })
-
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       '/api': {
-//         target: 'http://localhost:3000',
-//         changeOrigin: true,
-//         secure: false,
-//       },
-//     },
-//   },
-// })
+import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // Adiciona compressão Gzip aos arquivos estáticos gerados no build
+    viteCompression({
+      algorithm: 'gzip',
+      ext: '.gz',
+    })
+  ],
   server: {
     port: 5173,
-    host: true, // Necessário para Docker
+    host: true,
     proxy: {
-      // Toda requisição que começar com /api será enviada para o container 'backend'
       '/api': {
-        target: 'http://backend:3000', // 'backend' é o nome do serviço no docker-compose
+        target: 'http://backend:3000',
         changeOrigin: true,
-        // rewrite: (path) => path.replace(/^\/api/, '') // Remove o '/api' antes de enviar, se suas rotas no back não tiverem '/api'
-        // OBS: Se suas rotas no backend JÁ começam com /api (ex: router.get('/api/games')), REMOVA a linha 'rewrite' acima.
+        // rewrite: (path) => path.replace(/^\/api/, '') 
       }
     }
   }
